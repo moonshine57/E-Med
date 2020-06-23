7b77178a9375534bdc9f51a2c204e13d4c376757
sora <1034685115@qq.com>
2020-06-23 17:00:29 +0800

    api

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export interface AppConfig {
   }  
 
 export const CONFIG: AppConfig = {
-    API_ENDPOINT : "http://120.24.164.113:8103/api/" 
+    API_ENDPOINT : "http://120.24.164.113:8080/api/" 
   };
 
 //https://conduit.productionready.io/api/
