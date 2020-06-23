73179c8a41169e0e02ccb449fa191fef87e85be3
moonshine57 <1242727205@qq.com>
2020-06-23 09:44:41 +0800

    删除jupyter文件

--- src/.ipynb_checkpoints/constants-checkpoint.ts
+++
@@ -1,9 +0,0 @@
-export interface AppConfig {    
-    API_ENDPOINT : string   
-  }  
-
-export const CONFIG: AppConfig = {
-    API_ENDPOINT : "https://conduit.productionready.io/api/" 
-  };
-
-//https://conduit.productionready.io/api/
