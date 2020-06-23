b03ff894a70a75ff2cb95a075abc92472c06c1d6
sora <1034685115@qq.com>
2020-06-23 22:36:14 +0800

    外壳大致做完了

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {  IonContent,  IonSegment, IonSegmentButton, IonLabel,  IonList, IonAvatar, IonItem, IonIcon, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption} from '@ionic/react'
+import {  IonContent,  IonSegment, IonSegmentButton,IonButton, IonLabel,  IonList, IonAvatar, IonItem, IonIcon, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption,IonFooter,IonToolbar,IonButtons,IonPage} from '@ionic/react'
 import { Link } from 'react-router-dom';
 import CartCard from '../components/CartCard';
 import { TagCloud } from '../components/TagCloud';
 import Header from '../components/Header';
 import { CONFIG } from '../constants';
 import image from '../assets/images/商品图片.jpg';
+
 
 type Props = { props:any };
 type State = { carts: Array<any>, segment: string};
@@ -38,25 +39,25 @@ class ShoppingCartPage extends React.Component<Props, State> {
         }
       )
   }
- 
+
+
+
  card() {
   let url = CONFIG.API_ENDPOINT+"carts"
-   return (                 
+   return (      
+         
            <IonItem>
               <img src={image} slot="start" width = '40%'/>              
               <IonGrid >
                 <IonRow>
                   <IonCol size="8">
-                  <Link className="link" to={url}>
+                  <Link className="sname" to={url}>
                   同仁堂药店</Link>
-                </IonCol >
-                <IonCol size="4" text-right>
-                 <span className="psum">1</span>
-                </IonCol>
+                  </IonCol >
                 </IonRow>
                 
                <IonRow>
-                <Link className="link" to={url} text-left>同仁堂感冒灵颗粒</Link>              
+                <Link className="pname" to={url} text-left>同仁堂感冒灵颗粒</Link>              
                 </IonRow>
                
                  <IonRow> 
@@ -64,21 +65,30 @@ class ShoppingCartPage extends React.Component<Props, State> {
                   <p className="price" >￥25.86</p>        
                   </IonCol>
                 </IonRow>
+                
+               <IonRow> 
+                <IonCol size="6" text-right>
+                 <span className="psum">数量：1</span>
+                </IonCol>
+                  <IonButton color="white" text-center>                 
+                  <p className="delete">删除</p>        
+                  </IonButton>   
+                </IonRow>
               
               </IonGrid>
             
-          </IonItem>    
+          </IonItem>   
+ 
   )
-}  
-
-   
-
-  render() {      
-      return (
-        <>   
+} 
+ 
+   render() {      
+        return (
+        <> 
+        <IonPage>
         <Header title="购物车"></Header>
-        <IonContent> 
-          
+
+        <IonContent>  
         <IonList>
          {this.card()}
          {this.state.carts.map((article: any) => 
@@ -86,13 +96,23 @@ class ShoppingCartPage extends React.Component<Props, State> {
         )}
          <IonItem><p>  </p></IonItem>
          {this.card()}
-        </IonList>
-          
-        </IonContent>    
-      </>
+        </IonList>  
+        </IonContent> 
+        <IonFooter>
+          <IonToolbar>
+              <IonButtons slot="start">
+              <IonButton color="warning" fill = 'solid'>全选</IonButton>
+              </IonButtons>
+               <p className="price" slot="end" >总价</p>
+              <IonButtons slot="end">
+              <IonButton color="danger" fill = 'solid'>结算</IonButton>
+              </IonButtons>
+          </IonToolbar>
+         </IonFooter>
+         </IonPage>
+          </>
       );
     }
   
 }
-
 export default ShoppingCartPage
