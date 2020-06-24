4498e62b6903c0086dd006d1f1e83cb7e26f2e64
sora <1034685115@qq.com>
2020-06-24 22:38:37 +0800

    改成了原api

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,8 @@ export interface AppConfig {
   }  
 
 export const CONFIG: AppConfig = {
-    API_ENDPOINT : "http://120.24.164.113:8080/api/" 
+    API_ENDPOINT : "https://conduit.productionready.io/api/" 
   };
 
 //https://conduit.productionready.io/api/
+//http://120.24.164.113:8080/api/
