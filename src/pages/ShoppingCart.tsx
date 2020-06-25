288a5ddca500d7deae01ca5c337ea605d808f70b
sora <1034685115@qq.com>
2020-06-25 21:43:03 +0800

    购物车页面

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -8,15 +8,15 @@ import image from '../assets/images/商品图片.jpg';
 
 
 type Props = { props:any };
-type State = { articles: Array<any>, segment: string};
-
+type State = { articles: Array<any>, segment: string,sumprice:number};
 
 class ShoppingCartPage extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
     this.state = {      
       articles: [],              
-      segment: "cart"
+      segment: "cart",
+      sumprice:0.00
     };    
  
   }  
@@ -29,7 +29,8 @@ class ShoppingCartPage extends React.Component<Props, State> {
         (res) => {
           this.setState({           
             articles: res.articles,
-            segment: "cart"
+            segment: "cart",
+            sumprice:0.00
           });
         
         },
@@ -41,10 +42,6 @@ class ShoppingCartPage extends React.Component<Props, State> {
   }
 
 
-
- 
-
- 
    render() { 
         return (
         <> 
@@ -56,16 +53,16 @@ class ShoppingCartPage extends React.Component<Props, State> {
         {/*this.state.articles.map((article: any) => 
         <CartCard key={article.slug} title={article.title} src={article.author.image} description={article.description} favorited={article.favorited} favoritesCount={article.favoritesCount} slug={article.slug} author={article.author.username} checkbox={article.checkbox} incart={article.incart}></CartCard>)*/}
          {this.state.articles.map((article: any) =>
-          <CartCard checkbox={article.checkbox} psum={article.psum} incart={article.incart}></CartCard>)}
+          <CartCard checkbox={article.checkbox} psum={article.psum} incart={article.incart} sumprice={article.sumprice}></CartCard>)}
          <IonItem><p>  </p></IonItem>
               </IonList> 
           </IonContent> 
           <IonFooter>
           <IonToolbar>
               <IonButtons slot="start">
-              <IonButton color="warning" fill = 'solid'>全选</IonButton>
+              <IonButton color="warning" fill ='solid' >全选</IonButton>
               </IonButtons>
-               <p className="price" slot="end" >总价</p>
+               <p className="price" slot="end" >{this.state.sumprice}</p>
               <IonButtons slot="end">
               <IonButton color="danger" fill = 'solid'>结算</IonButton>
               </IonButtons>
