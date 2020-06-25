237e2e6ab0564da1b5eaa2624f9876f00d13a38b
sora <1034685115@qq.com>
2020-06-25 18:08:41 +0800

    添加减少商品

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import {  IonAvatar, IonItem, IonIcon, IonLabel, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonCheckbox} from '@ionic/react'
+import {  IonAvatar, IonItem, IonIcon, IonLabel, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonCheckbox, IonList,IonFooter,IonToolbar,IonButtons,IonContent} from '@ionic/react'
 import { Link } from 'react-router-dom';
 import './CartCard.css';
 import { CONFIG } from '../constants';
 import image from '../assets/images/商品图片.jpg';
+import {remove,add} from 'ionicons/icons';
 
 
 type Props = {  
-  title: string,
+ /*title: string,
   src: string,
+  slug: string,
+  author: string ,
   description: string,
   favorited: boolean,
-  favoritesCount: number,
-  slug: string,
+  favoritesCount: number,*/
   checkbox: boolean,
-  incart: boolean,
-  author: string  
+  psum:number,
+  incart: boolean
 }
 
 type State = {  
-  favorited: boolean,
+ /* favorited: boolean,
+  favoritesCount: number,*/
   checkbox: boolean,
-  incart: boolean,
-  favoritesCount: number
+  psum:number,
+  incart: boolean
 }
+
 
 class CartCard extends React.Component<Props, State> {
 
@@ -31,19 +35,22 @@ class CartCard extends React.Component<Props, State> {
     super(props);
 
     this.state = {      
-      favorited: this.props.favorited,
+     /* favorited: this.props.favorited,
+      favoritesCount: this.props.favoritesCount,*/
       checkbox:false,
-      incart:true,
-      favoritesCount: this.props.favoritesCount
+      psum:1,
+      incart:true
+      
     }
-    this.routeLink = '/article/'+this.props.slug;
-    this.profileLink = '/profile/'+this.props.author;
-    
+   /* this.routeLink = '/article/'+this.props.slug;
+    this.profileLink = '/profile/'+this.props.author;*/
+   
 
   }
-  routeLink: string;
-  profileLink: string;
- favoriteArticle = (params: any) => {
+
+/*  routeLink: string;
+  profileLink: string;*/
+/* favoriteArticle = (params: any) => {
  
   let url = CONFIG.API_ENDPOINT+"articles/" + this.props.slug + '/favorite';
   let method;
@@ -73,8 +80,8 @@ class CartCard extends React.Component<Props, State> {
         console.error(err);
       }
     )
-}
-
+}*/
+/*
 loggedInCard(){
   return (        
            <IonItemSliding>
@@ -109,19 +116,29 @@ loggedOutCard() {
             </IonLabel>
           </IonItem>      
   )
-}  
+}  */
  totalprice = () => {
-    let sum="0";
-    if(this.state.checkbox==true)
-     {sum+="25.86"}
-     else{sum+="-25.86"}
+   let sum=0
+   if(this.state.checkbox==true)
+     {sum=sum+25.86}
+     else{sum=sum-25.86}
    return {sum}
   }
  toggleAction = () => {
-    this.state.checkbox === false ? this.setState({checkbox: true}) : this.setState({checkbox: false})
+    this.setState({checkbox: true})
+   
   }
   deleteAction = () => {
-     this.state.incart === true ? this.setState({incart: false}) : this.setState({incart: true})
+   this.setState({incart: false})
+  }
+ addAction = () => {
+  this.setState({psum:this.state.psum+1})  
+  }
+ removeAction = () => {
+  if(this.state.psum===1)
+   {this.deleteAction()}
+  else
+  {this.setState({psum:this.state.psum-1})  }
   }
 
 card() {
@@ -152,9 +169,17 @@ card() {
                 </IonRow>
              {this.state.incart === true ?
                <IonRow> 
-                <IonCol size="8" text-right>
-                 <span className="psum">数量：1</span>
-                </IonCol>
+                  <IonButton color="white" onClick={this.removeAction}>
+                     <IonIcon icon = {remove} color="danger"></IonIcon>
+                   </IonButton>
+              {/*<IonCol size="1" text-center>
+                 <a className="psum">{1}</a>
+                </IonCol>*/}
+                 <a className="pm">1</a>
+                 <a className="psum">{this.state.psum}</a>
+                  <IonButton color="white" onClick={this.addAction}>
+                     <IonIcon icon = {add} color="danger"></IonIcon>
+                   </IonButton>
               
                   <IonButton color="white" text-center onClick={this.deleteAction}>               
                   <p className="delete">删除</p>        
@@ -163,7 +188,7 @@ card() {
                  : <><p className="delete">已删除</p></> }
               </IonGrid> 
             {this.state.incart === true ?
-             <IonCheckbox slot="end" value="pid" checked={false}/> : <></> }
+             <IonCheckbox slot="end" value="pid" checked={this.state.checkbox} onIonFocus={this.toggleAction}/> : <></> }
 
           </IonItem> 
         
@@ -175,7 +200,7 @@ card() {
         <>
         {this.card()} <IonItem><p>  </p></IonItem>
         {/*localStorage.getItem("isLogin") === "true" ? this.loggedInCard() : this.loggedOutCard()*/} 
-        </>               
+      </>               
       );    
   }
 }
