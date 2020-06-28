8ce123efc0e2f7eac1b06d184e7ef9b6b01cc84a
helloworld <hellohell>
2020-06-28 09:06:49 +0800

    上传商品页面联通

--- src/pages/NewArticle.tsx
+++ src/pages/NewArticle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  IonContent, IonButton, IonInput, IonToast } from '@ionic/react';
+import { IonItem,IonSelect,IonSelectOption,IonChip, IonIcon, IonLabel, IonContent, IonButton, IonInput, IonToast} from '@ionic/react';
 import ReactMde from "react-mde";
 import "react-mde/lib/styles/css/react-mde-all.css"; 
 import * as Showdown from "showdown"; 
@@ -7,12 +7,31 @@ import './Article.css';
 import { RouteComponentProps } from 'react-router';
 import Header from '../components/Header';
 import { CONFIG } from '../constants';
-
+import {image} from 'ionicons/icons';
+//import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 type Props = { props: {
     match: any
 } };
-type State = { error: any, isLoaded: boolean, value: any, tab: any, articleBody: string, description: String, title: String, tags: Array<string>,toastState: boolean};
+type State = { 
+    error: any, 
+    isLoaded: boolean, 
+    value: any, 
+    tab: any, 
+    price: string, //价格
+    category: string,//分类
+    name:string,//药品名
+    size:string,//规范
+    symptoms: string,//适用症状
+    usage:string,//适用方法
+    para:string,//成分
+    class: string,//处方药
+    problems:string,//相关问题
+    address: string,//发货地
+    stock: string,//库存
+    tags: Array<string>,//标签
+    toastState: boolean,
+    };
 
 class NewArticlePage extends React.Component<Props & RouteComponentProps, State>    {
     constructor(props: any) {
@@ -22,11 +41,19 @@ class NewArticlePage extends React.Component<Props & RouteComponentProps, State>
           isLoaded: false,                   
           value: "Write your article",
           tab: "write",
-          articleBody: '',
-          description: '',
+          price: '',
+	  category:'',
+          name:'',
+          size:'',
+          symptoms:'',
+          usage:'',
+          para:'',
+          class: '',
+          problems:'',
           tags: [],
-          title: '',
-          toastState: false
+          stock:'',
+          address:'',
+          toastState: false,
         };
        
         this.setEditor = (editor: any) => {
@@ -43,7 +70,7 @@ class NewArticlePage extends React.Component<Props & RouteComponentProps, State>
           simplifiedAutoLink: true,
           strikethrough: true,
           tasklists: true
-        });      
+        });
         
      
       }
@@ -52,56 +79,89 @@ class NewArticlePage extends React.Component<Props & RouteComponentProps, State>
       editor:any;
       focusEditor: any;
       converter: any;
-      handleBodyChange = (articleBody: any) => {
-        this.setState({ articleBody });
+      handleBodyChange = (problems: any) => {
+        this.setState({ problems });
       };
       handleTabChange = (tab: any) => {
         this.setState({ tab });
       };
-      titleChange = (event: CustomEvent) => {
-        
-        this.setState({ title: event.detail.value });
-
+      nameChange = (event: CustomEvent) => {    
+        this.setState({ name: event.detail.value });
       }
-      descriptionChange = (event: CustomEvent) => {
-       
-        this.setState({ description : event.detail.value });
-          
+      priceChange = (event: CustomEvent) => {
+        this.setState({ price : event.detail.value });
+      }
+      categoryChange = (event: CustomEvent) => { 
+        this.setState({ category: event.detail.value });
+      }
+      sizeChange = (event: CustomEvent) => { 
+        this.setState({ size: event.detail.value });
+      }
+      symChange = (event: CustomEvent) => { 
+        this.setState({ symptoms: event.detail.value });
+      }
+      useChange = (event: CustomEvent) => { 
+        this.setState({ usage: event.detail.value });
+      }
+      paraChange = (event: CustomEvent) => { 
+        this.setState({ para: event.detail.value });
+      }
+      addressChange = (event: CustomEvent) => {
+        this.setState({address: event.detail.value});
+      }
+      stockChange = (event: CustomEvent) => {
+        this.setState({stock: event.detail.value});
+      }
+      classchange = (event: CustomEvent) => {
+        this.setState({class: event.detail.value});
+        console.log(this.state.class);
       }
       tagsChange = (event: any) => {
-          
-         let tags =  (event.target as HTMLInputElement).value.split(',');
+        let tags =  (event.target as HTMLInputElement).value.split(',');
         this.setState({ tags: tags });
-          
-      }
-      submitArticle = (tag: any) =>{ 
-         
-          let articleData = {
-            "article": {
-                "title": this.state.title,
-                "description": this.state.description,
-                "body": this.state.articleBody,
-                "tagList": this.state.tags
+      } 
+      submitProduct = (tag: any) =>{ 
+          let productData = {
+            "pro": {
+                "category":this.state.category,
+                "pname": this.state.name,
+                "class": this.state.class,
+                "price": this.state.price,
+                "psize": this.state.size,
+                "symptoms": this.state.symptoms,
+                "usage": this.state.usage,
+                "para": this.state.para,
+                "problems": this.state.problems,
+                "pkeyword": this.state.tags,
+                "address": this.state.address,
+                "stock": this.state.stock,
             }
           }        
-        fetch(CONFIG.API_ENDPOINT+"articles", {
+        fetch(CONFIG.API_ENDPOINT+"pro_up/up/", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Token "+ localStorage.getItem("token") ,
             },
-            body: JSON.stringify(articleData)
+            body: JSON.stringify(productData)
 
         })
         .then(res => res.json())
         .then(
           (result) => {
-                  
               this.setState({
                 toastState: true,
-                title: "",
-                description: "",
-                articleBody: "",
+                name: "",
+                category:"",
+                ptype:"",
+                price: "",
+                size:"",
+                para:"",
+                pusage:"",
+                symptoms:"",
+                address:"",
+                stock:"",
+                problems: "",
                 tags: []
               })       
 
@@ -118,27 +178,46 @@ class NewArticlePage extends React.Component<Props & RouteComponentProps, State>
              <IonToast
         isOpen={this.state.toastState}
         onDidDismiss={() => this.setState(() => ({ toastState: false }))}
-        message= "Article Submitted"
+        message= "上架商品"
         duration={400}
       ></IonToast>
-    <Header title="New Article" />     
+    <Header title="上架商品" />     
     <IonContent>        
-    <form onSubmit={this.submitArticle}>
-      <IonInput type="text" placeholder="Title" onIonChange={this.titleChange} class="border-input"></IonInput>
-    <IonInput type="text" placeholder="What's this article about"  onIonChange={this.descriptionChange} class="border-input"></IonInput>
-  
+    <form onSubmit={this.submitProduct}>
+    <IonInput type="text" placeholder="药品名" onIonChange={this.nameChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="所属分类" onIonChange={this.categoryChange} class="border-input"></IonInput>
+    <IonInput type="number" placeholder="库存量" onIonChange={this.stockChange} class="border-input"></IonInput>
+    <IonItem>
+        <IonLabel>处方药/非处方药</IonLabel>
+        <IonSelect placeholder="选择是否为处方药" onIonChange={this.classchange}>
+           <IonSelectOption value="处方药">处方药</IonSelectOption>
+           <IonSelectOption value="非处方药">非处方药</IonSelectOption>
+        </IonSelect>
+    </IonItem>
+    <IonInput type="text" placeholder="药品价格" onIonChange={this.priceChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="药品规格" onIonChange={this.sizeChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="适用症状" onIonChange={this.symChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="使用方法" onIonChange={this.useChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="主要成分" onIonChange={this.paraChange} class="border-input"></IonInput>
+    <IonInput type="text" placeholder="发货地" onIonChange={this.addressChange} class="border-input"></IonInput>
+    <IonInput type="number" placeholder="库存量" onIonChange={this.stockChange} class="border-input"></IonInput>
+    <IonChip>
+        <IonIcon icon={image} />
+    	<IonLabel>上传药品照片</IonLabel>
+    </IonChip>
+    <p>请输入商品常见问题:</p>
           <ReactMde
           onChange={this.handleBodyChange}
           onTabChange={this.handleTabChange}
-          value={this.state.articleBody}
+          value={this.state.problems}
           selectedTab={this.state.tab}
           generateMarkdownPreview={markdown =>
-            Promise.resolve(this.converter.makeHtml(markdown))
+          Promise.resolve(this.converter.makeHtml(markdown))
           }
         />
-         <IonInput type="text"  placeholder="Enter Tags" class="border-input"  onIonChange={this.tagsChange}></IonInput>
-         <IonButton expand="block" onClick={this.submitArticle}>Submit Article</IonButton>        
-      </form>      
+         <IonInput type="text"  placeholder="输入标签" class="border-input"  onIonChange={this.tagsChange}></IonInput>
+         <IonButton expand="block" onClick={this.submitProduct}>提交商品</IonButton>        
+      </form>
     </IonContent>
   </>
         )
