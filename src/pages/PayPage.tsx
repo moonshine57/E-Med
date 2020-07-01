f217d792e6f7c2242315a51151db6b4f886d7d7b
moonshine57 <1242727205@qq.com>
2020-07-01 20:04:50 +0800

    与后端调通新增地址功能

--- src/pages/PayPage.tsx
+++ src/pages/PayPage.tsx
@@ -3,9 +3,10 @@ import {IonAlert,IonPage,IonFooter,IonToolbar,IonButtons, IonGrid, IonRow, IonCo
 import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 import image from '../assets/images/商品图片.jpg';
+import { CONFIG } from '../constants';
 
 type Props = { props: any };
-type State = {username: string, password: string, bio: string, image: string, email: string, toastState: boolean,showAlert:boolean };
+type State = {receiveInfo:Array<any>,username: string, password: string,toastState: boolean,showAlert:boolean };
 
 class PayPage extends React.Component<Props, State> {
 
@@ -14,15 +15,39 @@ class PayPage extends React.Component<Props, State> {
         this.state = {
             username: '',
             password: '',
-            bio: '',
-            image: '',
-            email: '',
+            receiveInfo:[],
             toastState:false,
             showAlert: false,
             
         };
 
     }
+
+    componentWillMount() {
+        let url = CONFIG.API_ENDPOINT + 'user_md/getreceiveinfo/';
+        fetch(url, {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "" + localStorage.getItem("token")
+          }
+        })
+          .then((res) => {
+            if(res.status == 200){
+              return res.json();
+           }
+           else{
+             throw new Error();
+           }
+          }
+          ).then((res)=>{
+            res = JSON.parse(res);
+            this.setState({receiveInfo:res})
+          },
+            (error) => {
+              this.setState({receiveInfo:[]})
+            })
+      }
 
     render() {
         let url = "http"
@@ -41,13 +66,13 @@ class PayPage extends React.Component<Props, State> {
                             </IonRow>
 
                             <IonRow>
-                                <p className="name" text-left>同仁堂感冒灵颗粒</p>
+                                <p className="name" >同仁堂感冒灵颗粒</p>
                             </IonRow>
                             <IonRow>
 
                             </IonRow>
                             <IonRow>
-                                <IonCol size="6" text-left>
+                                <IonCol size="6" >
                                     <p className="price" >￥25.86*1</p>
                                 </IonCol>
                             </IonRow>
@@ -57,13 +82,18 @@ class PayPage extends React.Component<Props, State> {
                     <IonItem>
                         <IonLabel>选择收货地址</IonLabel>
                         <IonSelect interface="action-sheet">
-                            <IonSelectOption >广东省广州市大学城中山大学至善园八号306宿舍</IonSelectOption>
-                            <IonSelectOption>Brown</IonSelectOption>
-                            <IonSelectOption>Dark</IonSelectOption>
+                        {this.state.receiveInfo.map((info: any,index) => 
+                            <IonSelectOption key = {index}>
+                                {info.raddress}{" \n "}
+                                {info.rname}{" \n "}
+                                {info.rphone}
+                            </IonSelectOption>)}
                         </IonSelect>
                     </IonItem>
                     <section>
-                        <IonButton slot = "end">添加收货地址</IonButton>
+                    <Link className="link" to={{
+                        pathname:'/address',
+                        }}><IonButton slot="end">添加收货地址</IonButton></Link> 
                     </section>
                     <IonItem>
                         <IonLabel>选择支付方式</IonLabel>
