7071ac3bd474445265f151fe355840a4530edb75
sora <1034685115@qq.com>
2020-07-08 22:40:24 +0800

    商家订单管理内容

---
+++ src/components/OrdProCard.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {  IonAvatar, IonItem, IonIcon, IonLabel, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonCheckbox, IonList,IonFooter,IonToolbar,IonButtons,IonContent} from '@ionic/react'
+import { Link } from 'react-router-dom';
+import './SellerOrder.css';
+import { CONFIG } from '../constants';
+import image from '../assets/images/商品图片.jpg';
+import {remove,add} from 'ionicons/icons';
+
+
+type Props = {  
+  pid:number,
+  pname: string,
+  psum: number
+ 
+}
+type State = {  
+
+}
+
+
+
+class OrdProCard extends React.Component<Props, State> {
+
+  constructor(props: Props){
+    super(props);
+
+  }
+  
+
+/* favoriteArticle = (params: any) => {
+ 
+  let url = CONFIG.API_ENDPOINT+"articles/" + this.props.slug + '/favorite';
+  let method;
+  if (!this.state.favorited) {
+    method = 'POST'
+  } else {
+    method = "DELETE"
+  }
+  fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Token " + localStorage.getItem("token"),
+      }
+    })
+    .then(res => res.json())
+    .then(
+      (res) => {       
+        this.setState({
+          favorited: res.article.favorited,
+          checkbox:false,
+          incart:true,
+          favoritesCount: res.article.favoritesCount,
+        })
+      },
+      (err) => {
+        console.error(err);
+      }
+    )
+}*/
+/*
+loggedInCard(){
+  return (        
+           <IonItemSliding>
+          {this.loggedOutCard()}
+        <IonItemOptions side="end">
+          <IonItemOption  color={this.state.favorited ? "success": "light"} onClick={this.favoriteArticle}>
+          <IonIcon color={this.state.favorited ? "light": "success"} class="icon-blog-card" name="heart" />{this.state.favoritesCount}</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>           
+  )
+}
+
+loggedOutCard() {
+  return (                 
+           <IonItem >
+          <IonAvatar slot="start">
+              <img src={this.props.src} />              
+            </IonAvatar>
+            <IonLabel>
+              <p className="title">{this.props.title}</p>              
+              <IonGrid >
+                <IonRow>
+                  <IonCol class="author" size="6">
+                  <Link className="link" to={this.profileLink}>
+                  {this.props.author}</Link>                     
+                 </IonCol>
+                  <IonCol  size="6" text-right>                  
+                  <Link className="link" to={this.routeLink}>Read More</Link>        
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonLabel>
+          </IonItem>      
+  )
+}  */
+
+ /*toggleAction = () => {
+   this.state.checkbox===false? this.setState({checkbox: true}):this.setState({checkbox: false})
+  }*/
+  
+card(){
+  let purl ="product/"+this.props.pid
+   return (  
+   
+           <IonItem>
+            
+              <IonGrid >
+               
+               <IonRow>
+                <Link className="opname" to={purl} text-left>{this.props.pname}</Link> 
+                  <p className="pm" >数量：{this.props.psum}</p> 
+                <p className="opsum">数量：{this.props.psum}</p>  
+                </IonRow>
+             </IonGrid> 
+          
+
+          </IonItem> 
+        
+  )
+}  
+
+  render() {   
+      return (
+        <>
+        {this.card()}
+        {/*localStorage.getItem("isLogin") === "true" ? this.loggedInCard() : this.loggedOutCard()*/} 
+      </>               
+      );    
+  }
+}
+
+export default OrdProCard
