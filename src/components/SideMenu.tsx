fe8c871318dd88d1724e2cb1ede04e40e9ee5096
sora <1034685115@qq.com>
2020-07-08 16:06:13 +0800

    侧边栏

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -13,14 +13,13 @@ class SideMenu extends React.Component<any, any> {
           { title: '首页', path: '/', icon: 'home' },         
         ],
         loggedInPages: [
-          { title: '我的', path: '/profile/'+localStorage.getItem("username"), icon: 'person'},        
-          { title: '购物车', path: '/shoppingcart', icon: 'cart' },
+          { title: '个人中心', path: '/buyerprofile/'+localStorage.getItem("username"), icon: 'person'},             { title: '购物车', path: '/shoppingcart', icon: 'cart' },
           { title: '设置', path: '/settings', icon: 'settings' },
           { title: '退出登录', path: '/login', icon: 'log-out' }
         ],
         loggedOutPages: [
           { title: '登录', path: '/login', icon: 'log-in' },
-          { title: '药店进驻', path: '/sellerlogin', icon: 'log-in' }
+          { title: '药店登录', path: '/sellerlogin', icon: 'log-in' }
         ] ,
       }
     }
@@ -32,16 +31,15 @@ class SideMenu extends React.Component<any, any> {
             { title: '首页', path: '/', icon: 'home' },         
           ],
           loggedInPages: [
-            { title: '我的', path: '/profile/'+localStorage.getItem("username"), icon: 'person'},        
-            { title: '购物车', path: '/shoppingcart', icon: 'cart' },
+            { title: '个人中心', path: '/buyerprofile/'+localStorage.getItem("username"), icon: 'person'},             { title: '购物车', path: '/shoppingcart', icon: 'cart' },
             { title: '设置', path: '/settings', icon: 'settings' },
             { title: '退出登录', path: '/login', icon: 'log-out' }
           ],
           loggedOutPages: [
             { title: '登录', path: '/login', icon: 'log-in' },
-            { title: '药店进驻', path: '/sellerlogin', icon: 'log-in' },
-            { title: '医师进驻', path: '/DoctorLogin', icon: 'log-in' },
-            { title: '管理员登陆', path: '/ManagerLogin', icon: 'log-in' }
+            { title: '药店登录', path: '/sellerlogin', icon: 'log-in' },
+            { title: '医师登录', path: '/DoctorLogin', icon: 'log-in' },
+            { title: '管理员登录', path: '/ManagerLogin', icon: 'log-in' }
           ],
        
         }        
