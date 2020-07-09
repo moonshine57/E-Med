9e5775c91ea9c3c0980382f99e46453a5709a2f0
moonshine57 <1242727205@qq.com>
2020-07-09 15:28:27 +0800

    修改订单审核第一重card

--- src/components/checklist_doc.tsx
+++ src/components/checklist_doc.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import {   IonCard, IonCardContent,IonAvatar, IonItem, IonIcon, IonLabel, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonCheckbox, IonList,IonFooter,IonToolbar,IonButtons,IonContent} from '@ionic/react'
+import {   IonInput,IonRadio,IonCard, IonCardContent,IonAvatar, IonItem, IonIcon, IonLabel, IonGrid, IonCol, IonRow, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonCheckbox, IonList,IonFooter,IonToolbar,IonButtons,IonContent} from '@ionic/react'
 import { Link } from 'react-router-dom';
 import './goodlist.css';
 import { CONFIG } from '../constants';
 import image from '../assets/images/商品图片.jpg';
 import {remove,add} from 'ionicons/icons';
-
-
+import DocProCard from '../components/DocProCard';
+import {document,eye,home } from 'ionicons/icons';
 type Props = {  
-  did: number,
-  dprove: string,
-  dname: string,
-  dtitle:string,
+  ordno: number,
+//  pid: number,
+ // pname: string,
+  //pro:[]
+   pro: Array<any>,
+   image:string
 }
 
 type State = {  
- 
+ pro: Array<any>,
 }
 
 
@@ -25,15 +27,30 @@ class CheckListDoc extends React.Component<Props, State> {
     super(props);
 
     this.state = {      
-   
-      incart:true
+    pro:[]
+     //pro:[{pid: 4, pname: "药1"}, {pid: 5, pname: "药2"}],
     }
   }
 
-checkAction = () => {
+TrueAction = () => {
   console.log("勾选事件")
-  let url = CONFIG.API_ENDPOINT+"XXXXXX"
-  let checkcart = {"did":this.props.did}
+  let url = CONFIG.API_ENDPOINT+"order_md/checkRx/"
+  let checkcart = {"ordno":this.props.ordno,"result":"通过"}
+  fetch(url, {
+      method: 'POST',
+      headers: {
+         "Content-Type": "application/json", 
+         "Authorization": ""+localStorage.getItem("token")
+      },
+       body: JSON.stringify(checkcart)
+    })
+  
+  }
+
+ FalseAction = () => {
+  console.log("勾选事件")
+  let url = CONFIG.API_ENDPOINT+"order_md/checkRx/"
+  let checkcart = {"ordno":this.props.ordno,"result":"不通过"}
   fetch(url, {
       method: 'POST',
       headers: {
@@ -46,15 +63,27 @@ checkAction = () => {
   }
 
 card(){
-
+ 
    return (  
-      
+      <>
+      <IonIcon icon = {document}></IonIcon> 
+     <IonLabel >   通过       不通过</IonLabel> 
        <IonItem>
-        <IonLabel>资质证明号：{this.props.dprove}</IonLabel>
-        <IonLabel>医生姓名：{this.props.dname}</IonLabel>
-        <IonCheckbox slot="start"  onChange={this.checkAction}></IonCheckbox >
+        <IonCheckbox slot="start"  onClick={this.TrueAction}> </IonCheckbox >
+        <IonCheckbox slot="start"  onClick={this.FalseAction}>不通过</IonCheckbox >
+         <IonLabel>订单号：{this.props.ordno}</IonLabel>
+         <IonLabel>审核材料：{this.props.image}</IonLabel>
+        </IonItem>
+        <IonItem>
+        
+     
+           <IonList>
+               {this.props.pro.map((product: any) =>
+          <DocProCard key={product.pid} pid={product.pid} pname={product.pname}></DocProCard>)}
+            </IonList>
       </IonItem>
-   
+       
+   </>
   )
 }  
 
